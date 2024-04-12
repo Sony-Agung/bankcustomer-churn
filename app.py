0f37a77c6c91724417b2cbecf146a80ac92855cc
Sony Agung <124691368+Sony-Agung@users.noreply.github.com>
2024-04-13 01:17:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,124 +1,140 @@
+
+
+import requests
+
 import streamlit as st
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-import joblib
-from sklearn.ensemble import RandomForestClassifier
+from streamlit_lottie import st_lottie
+from PIL import Image
+import io
+import base64
 
-# Memisahkan fitur (X) dan target (y)
+st.set_page_config(
+    page_title="My_Portofolio",
+    page_icon="🏠",
+    layout="wide"
+)
 
 
-X_train=pd.read_csv('model/X_train.csv' ,usecols=lambda column: column not in ['Unnamed: 0', 'index'])
-y_train=pd.read_csv('model/y_train.csv' ,usecols=lambda column: column not in ['Unnamed: 0', 'index'])
-model = RandomForestClassifier()
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
-joblib.dump(model, 'model/random_forest_classifier.joblib')
 
-# Saat memuat model:
-model = joblib.load('model/random_forest_classifier.joblib')
-model.fit(X_train, y_train)
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-# Fungsi untuk halaman Home
-def home():
-    st.image('templates/curr.jpeg')
-    st.title("Welcome to Customer Churn Prediction")
+def resize_image(input_image_path, size):
+    original_image = Image.open(input_image_path)
+    width, height = original_image.size
+    aspect_ratio = width/height
+    new_height = size
+    new_width = int(new_height * aspect_ratio)
+    resized_image = original_image.resize((new_width, new_height))
+    return resized_image
 
-    # Tampilkan navigasi
-    st.write()
-    menu = ["Home", "About", "Prediction App"]
-    choice = st.sidebar.radio("", menu)
+local_css("style/style.css")
 
-    if choice == "Home":
-        # Tampilkan pesan selamat datang
-        st.write("""
-        Predict whether a customer will churn or be retained based on provided data.
-        """)
+lottie_coding = load_lottieurl("https://lottie.host/dbb6192e-e1c3-41ab-b02a-169e62282534/Z3wKX5HScH.json")
 
-    elif choice == "Prediction App":
-        # Create input form for the user to input data
-        st.sidebar.header("Input Customer Data")
+# ---- HEADER SECTION ----
+with st.container():
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.markdown('<h2 style="font-size: 45px;">Hi, I am Soni 👋</h2>', unsafe_allow_html=True)
+        st.markdown('<h2 style="font-size: 35px;">A Data Enthusiast</h2>', unsafe_allow_html=True)
+        st.markdown("""
+        <p style='text-align: justify;'>I am dedicated to honing my data analysis skills through web research, forum engagement, and self-directed
+        learning, alongside hands-on experience gained from boot camps. Proficient in Production Planning and
+        Inventory Control (PPIC), I excel in leveraging MS Excel for precise production forecasting and scheduling. As
+        an industrious student at Pelita Bangsa University, pursuing Industrial Engineering, I blend theoretical
+        knowledge with practical insights, fostering a promising career trajectory.
 
-        with st.sidebar.form("input_form"):
-            customer_id = st.number_input("Customer ID", value=72322212)
-            credit_score = st.number_input("Credit Score", value=645)
-            gender = st.radio("Gender", ["Male", "Female"])
-            age = st.number_input("Age", value=44)
-            tenure = st.number_input("Tenure", value=8)
-            balance = st.number_input("Balance", value=113755.78)
-            products_number = st.number_input("Number of Products", value=2)
-            credit_card = st.checkbox("Has Credit Card")
-            active_member = st.checkbox("Active Member")
-            estimated_salary = st.number_input("Estimated Salary", value=149756.71)
-            country = st.selectbox("Country", ["France", "Germany", "Spain"])
-            submit_button = st.form_submit_button("Predict")
+        Key Proficiencies: <b>Data Analysis | Python | Machine Learning | Google Spreadsheet | MS Excel | SQL | Google Data Studio</b>
 
-        # Create a DataFrame from the input data
-        data_baru = pd.DataFrame({
-            'customer_id': [customer_id],
-            'credit_score': [credit_score],
-            'gender': [gender],
-            'age': [age],
-            'tenure': [tenure],
-            'balance': [balance],
-            'products_number': [products_number],
-            'credit_card': [1 if credit_card else 0],
-            'active_member': [1 if active_member else 0],
-            'estimated_salary': [estimated_salary],
-            'France': [1 if country == "France" else 0],
-            'Germany': [1 if country == "Germany" else 0],
-            'Spain': [1 if country == "Spain" else 0]
-        })
+         
+        </p>
+        """, unsafe_allow_html=True)
+        st.markdown('<h2 style="font-size: 20px;">Dont forget to check the sidebar > for my portofolio </h2>', unsafe_allow_html=True)
+    with col2:
+        st.title(" ")
+    with col3:
+        st.title(" ")
+        st.title(" ")
+        st.title(" ")
+        st.title(" ")
+        resized_image = resize_image("images//Foto.PNG", 400)
+        st.image(resized_image)
 
-        data_baru = data_baru.drop('customer_id', axis=1)
 
-        # Data preprocessing
-        category_mappings = {
-            "gender": {"Male": 1, "Female": 0}
-        }
-        data_baru.replace(category_mappings, inplace=True)
 
-        # Define the feature columns used during training (replace with your actual feature names)
-        data_baru = data_baru[X_train.columns]
+with st.container():
+    st.write("---")
+    left_column, center_column, right_column = st.columns(3)
+    with left_column:
+        st.header("Work Experience")
+        st.markdown('<h2 style="font-size: 20px;">PPIC - PT. NIPPON INDOSARI CORPINDO    |  Cikarang, Indonesia  |   Dec 2022 -  Present</h2>', unsafe_allow_html=True)
+        st.markdown("""
+        <ul>
+            Has demonstrated effective forecasting to prevent excess or shortage of materials, thereby ensuring
+            cost efficiency in line with consumer demand. Developed an intuitive Google Data Studio dashboard
+            for tracking raw material expiration, enabling all staff to align with the FIFO method for outgoing goods
+            and swiftly identify and manage minimal stock levels accurately.
+        </ul>
+        """, unsafe_allow_html=True)
 
-        # Scale the new data using the same scaler used for training
-        scaler = StandardScaler()
+        st.markdown('<h2 style="font-size: 20px;">Data Analyst Intern - Rumah Kepemimpinan    |   Jakarta, Indonesia (Remote)  |   Nov 2023 - Dec 2023</h2>', unsafe_allow_html=True)
+        st.markdown("""
+        <ul>
+            <li>Data Cleaning : Processing unstructured donor data to be analyzed using Python (Jupyter)</li>
+            <li>Aggregating cleaned data to find insights</li>
+            <li>Creating an automated system for data processing from cleaning to data visualization to facilitate clients</li>
+            <li>Creating data visualizations using Looker Studio for collaboration and monitoring</li>
+            <li>Presenting the analysis results to clients</li>
+        </ul>
+        """, unsafe_allow_html=True)
 
-        # Fit the scaler on the training data (X_train) before transforming
-        scaler.fit(X_train)
+        st.markdown('<h2 style="font-size: 20px;">Bank Muamalat Business Intelligence Analyst -  Project Based Internship Program   |   Jakarta, Indonesia (Remote)  |   Feb 2024</h2>', unsafe_allow_html=True)
+        st.markdown("""
+        <ul>
+            <li>Data Cleaning : Performing data cleaning using Google BigQuery to structure the data</li>
+            <li>Aggregating using GBQ from cleaned data to find insights</li>
+            <li>Creating data visualizations using Looker Studio for collaboration and monitoring</li>
+            <li>Presenting the analysis results to clients and providing recommendations for further business decisions</li>
+        </ul>
+        """, unsafe_allow_html=True)
 
-        # Transform the new data (data_baru) using the fitted scaler
-        scaled_data_baru = scaler.transform(data_baru)
+    with right_column:
+        st.title(" ")
+        st.title(" ")
+        st_lottie(lottie_coding, height=500, key="coding")
+        
 
-        # Make predictions using the trained model when the form is submitted
-        if submit_button:
-            prediction = model.predict(scaled_data_baru)
-            probability = model.predict_proba(scaled_data_baru)
 
-            st.subheader("Prediction Result")
-            if prediction[0] == 1:
-                st.success("Customer: Churn")
-            else:
-                st.success("Customer: Retained")
+with st.container():
+    st.write("---")
+    left_column, center_column, right_column = st.columns(3)
+    with left_column:
+        st.header("Education")
+        st.markdown('<h2 style="font-size: 30spx;">Universitas Pelita Bangsa</h2>', unsafe_allow_html=True)
+        st.markdown('<p style="font-size: 20px;">Active Student in Industrial Engineering until 2026</p>', unsafe_allow_html=True)
+    with center_column:
+        st.title(" ")
+    with right_column:
+        image = Image.open("images//univ2.png")
+        resized_image = image.resize((200,200))
+        buffered = io.BytesIO()
+        resized_image.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        st.markdown(f'<img src="data:image/png;base64,{img_str}" style="margin-left: 30px;">', unsafe_allow_html=True)
 
-            st.subheader("Prediction Probabilities")
-            st.write(f"Probability of Churn: {probability[0][1]*100:.2f}%")
-            st.write(f"Probability of Retained: {probability[0][0]*100:.2f}%")
-
-    elif choice == "About":
-        # Tampilkan halaman About
-        st.title("About This App")
-        st.write("""
-        This web application is built to predict whether a customer will churn (leave) or be retained based on the provided customer data. It uses a trained machine learning model to make predictions.
-
-        - **Model**: Random Forest Classifier
-        - **Features**: Credit Score, Gender, Age, Tenure, Balance, Number of Products, Has Credit Card, Active Member, Estimated Salary, Country
-        - **Data Source**: Dummy data for demonstration purposes.
-
-        Feel free to use the input form to make predictions or explore other features of this app.
-        """)
-
-# Define the Streamlit app
-def main():
-    home()  # Tampilkan halaman Home secara default
-
-if __name__ == '__main__':
-    main()
+st.write("---")
+st.markdown('<h2 style="font-size: 30px;">Find Out More</h2>', unsafe_allow_html=True)
+st.write("[LinkedIn ](https://www.linkedin.com/in/soni-agung-wahyudiyanta-285901180?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BI9thWKaHQTCBlkEZhePWlA%3D%3D)")
+st.write("[Github ](https://github.com/Sony-Agung)")
+st.write("[Resume ](https://drive.google.com/file/d/1gZ-2Q2-M1VBVeAIGZ03Jo4d8e9fZ11sy/view?usp=sharing)")
+st.write("#")
+st.markdown('<h2 style="font-size: 30px;"> Dont forget to check the sidebar > for my portofolio </h2>', unsafe_allow_html=True)
